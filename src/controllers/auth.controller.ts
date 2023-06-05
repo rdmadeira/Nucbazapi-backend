7131380f2a3121126ad1749aa09349a70128b33e
rdmadeira <rdmadeira2@gmail.com>
2023-06-05 19:02:51 -0300

    api-nucba-zapi 2 Fin

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
 /* import { AuthResponse } from '../core/entities/Auth.js'; */
 import interactors from '../core/interactors/index.js';
+import { RequestValidatorError } from '../errors/request_validation_error.js';
 
 export const loginController = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) return next(new RequestValidatorError(errors.array()));
+
   const { email, password } = req.body;
   const loginResponse = await interactors.LoginAuthInteractor({
     email,
@@ -24,6 +30,11 @@ export const signinController = async (
   res: Response,
   next: NextFunction
 ) => {
+  const errors = validationResult(req);
+  console.log('errors', errors);
+
+  if (!errors.isEmpty()) return next(new RequestValidatorError(errors.array()));
+
   const { email, password, name } = req.body;
   const signInResponse = await interactors.SigninAuthInteractor({
     name,
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,10 +3,22 @@ import {
   loginController,
   signinController,
 } from '../controllers/auth.controller.js';
+import { body } from 'express-validator';
 
 const router = Router();
 
-router.post('/signin', signinController);
-router.post('/login', loginController);
+router.post(
+  '/signin',
+  body('name').trim().notEmpty().withMessage('Nombre obligatório'),
+  body('email').isEmail().withMessage('Ingrese un email válido'),
+  body('password').trim().notEmpty().withMessage('Contraseña obligatória'),
+  signinController
+);
+router.post(
+  '/login',
+  body('email').isEmail().withMessage('Ingrese un email válido'),
+  body('password').trim().notEmpty().withMessage('Contraseña obligatória'),
+  loginController
+);
 
 export default router;
